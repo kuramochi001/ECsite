ff88dcd64d5f558fb4fa989b098f4bcac4366216
kuramochi001 <natsumi.a13@gmail.com>
2019-04-05 21:43:40 +0900

    0405

--- HelloServlet/sql/testdb.sql
+++ HelloServlet/sql/testdb.sql
@@ -1,4 +1,4 @@
-drop detabase if exists testdb;
+drop database if exists testdb;
 create database testdb;
 use testdb;
 create table test_table(user_id int,user_name varchar(255),password varchar(255));
